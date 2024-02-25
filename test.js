8817eb81d090bac95531effc9ff6710247491286
reyang <didfodms@gmail.com>
2024-02-25 19:16:06 +0900

    edit

--- test.js
+++ test.js
@@ -6,7 +6,64 @@ const mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+const 데이터 = [
+    {
+        name : '사범대',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455302,
+        lng : 126.563627
+    },
+    {
+        name : '교양동',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455694,
+        lng : 126.564467
+    },
+    {
+        name : '아라홀',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455712,
+        lng : 126.562971
+    },
+    {
+        name : '야외음악당',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.457096,
+        lng : 126.562851
+    },
+    {
+        name : '본관',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455915,
+        lng : 126.561952
+    },
+    {
+        name : '운동장',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455380,
+        lng : 126.559676
+    },
+    {
+        name : '신관',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.455072,
+        lng : 126.560683
+    },
+    {
+        name : '외교원',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.454073,
+        lng : 126.560326
+    },
+    {
+        name : '중앙도서관',
+        imageSrc : './logo/marker_red.png',
+        lat : 33.452694,
+        lng : 126.560931
+    },
+]
+
+const imageSrc = './logo/marker_red.png', // 마커이미지의 주소입니다    
     imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
     imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
